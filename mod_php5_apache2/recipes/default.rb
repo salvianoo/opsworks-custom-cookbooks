58cd6b88f6f10acb6df393e252bbcddafbd78b39
Salviano Ludgerio <salviano.lfg@gmail.com>
2016-02-24 10:12:55 -0300

    Fix typo

--- mod_php5_apache2/recipes/default.rb
+++ mod_php5_apache2/recipes/default.rb
@@ -21,7 +21,7 @@ end
 node[:mod_php5_apache2][:packages].each do |pkg|
   package pkg do
     action :install
-    options '--froce-yes'
+    options '--force-yes'
     ignore_failure(pkg.to_s.match(/^php-pear-/) ? true : false) # some pear packages come from EPEL which is not always available
   end
 end
