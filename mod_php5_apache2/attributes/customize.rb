58dc1560e1810a3b54b7e84f613930dc0f5ea24c
Salviano Ludgerio <salviano.lfg@gmail.com>
2016-02-24 10:26:37 -0300

    Remove some php7 packages

--- mod_php5_apache2/attributes/customize.rb
+++ mod_php5_apache2/attributes/customize.rb
@@ -1,12 +1,7 @@
 normal[:mod_php5_apache2][:packages] = [
     "php7.0-xsl",
     "php7.0-curl",
-    "php7.0-xmlrpc",
-    "php7.0-sqlite",
-    "php7.0-dev",
     "php7.0-gd",
-    "php7.0-cli",
-    "php7.0-sasl",
     "php7.0-mcrypt",
     "php7.0-memcache"
 ]
