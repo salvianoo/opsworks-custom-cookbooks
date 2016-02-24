446e03079b37101ff66cfcc27f9604717c8019c1
Salviano Ludgerio <salviano.lfg@gmail.com>
2016-02-24 11:51:14 -0300

    Minor

--- mod_php5_apache2/recipes/mysql_adapter.rb
+++ mod_php5_apache2/recipes/mysql_adapter.rb
@@ -1,7 +1,7 @@
 package 'php-mysql' do
+  options '--force-yes'
   package_name value_for_platform_family(
     'rhel'   => 'php-mysql',
     'debian' => 'php7.0-mysql'
   )
-  options '--force-yes'
 end
